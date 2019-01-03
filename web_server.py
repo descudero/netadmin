da1b5cb7d0b4f64f71f1835155d2f4a8a36df01f
descudero <danielignacio.escudero@gmail.com>
2019-01-03 17:10:29 -0600

    first3

--- web_server.py
+++ web_server.py
@@ -14,11 +14,16 @@ mysql = MySQL(app)
 @app.route('/reportes/internet/')
 def reporte_internet():
     sql = '''select  
-       d.hostname,i.uid,i.if_index,
-       left(right(i.description,CHAR_LENGTH(i.description)-20),35) as description,i.l3_protocol,
-       i.l3_protocol_attr,i.l1_protocol,i.l1_protocol_attr,
+       d.hostname as host,
+       i.uid,
+       i.if_index as inter,
+       left(right(i.description,CHAR_LENGTH(i.description)-20),35) as description,i.l3_protocol as l3p,
+       i.l3_protocol_attr as l3a ,
+       i.l1_protocol ,
+       i.l1_protocol_attr,
        s.util_in as util_in,util_out as util_out,
-       (s.input_rate/1073741824) as input_rate_gbs ,(s.output_rate/1073741824) as output_rate_gbs
+       (s.input_rate/1073741824) as in_gbs ,
+       (s.output_rate/1073741824) as out_gbs
        ,s.state_timestamp  
        from network_devices as d
        inner join interfaces as i on d.uid = i.net_device_uid 
@@ -26,30 +31,31 @@ def reporte_internet():
        where s.state_timestamp >='2019-01-01 00:00:00' and s.state_timestamp <'2019-01-31 00:00:00'
        '''
     df = pd.read_sql(sql, con=mysql.connection)
-    df2 = df[['hostname', 'uid', 'if_index', 'description', 'l3_protocol', 'l3_protocol_attr', 'l1_protocol',
+    df2 = df[['host', 'uid', 'inter', 'description', 'l3p', 'l3a', 'l1_protocol',
               'l1_protocol_attr']].drop_duplicates(subset='uid', keep='first')
     df_proceded = (
-        df.groupby(by=['uid'])['util_out', 'util_in', 'output_rate_gbs', 'input_rate_gbs'].quantile(.95)).round(
+        df.groupby(by=['uid'])['util_out', 'util_in', 'out_gbs', 'in_gbs'].quantile(.95)).round(
         1).reset_index()
     df_merge = pd.merge(df_proceded, df2, on=['uid'], how='left').sort_values(ascending=False, by=['util_out'])
+    df_merge.drop(['uid'], axis=1, inplace=True)
 
-    ipts = df_merge[df_merge['l3_protocol_attr'] == 'IPT'].sort_values(by="util_in", ascending=False).to_dict(
+    ipts = df_merge[df_merge['l3a'] == 'IPT'].sort_values(by="util_in", ascending=False).to_dict(
         orient='records')
 
-    pnis = df_merge[df_merge['l3_protocol_attr'] == 'PNI'].sort_values(by="util_out", ascending=False).to_dict(
+    pnis = df_merge[df_merge['l3a'] == 'PNI'].sort_values(by="util_out", ascending=False).to_dict(
         orient='records')
 
-    ibps = df_merge[df_merge['l3_protocol_attr'] == 'IBP'].sort_values(by="util_out", ascending=False).to_dict(
+    ibps = df_merge[df_merge['l3a'] == 'IBP'].sort_values(by="util_out", ascending=False).to_dict(
         orient='records')
     mpls_cm = df_merge[
-        (df_merge['l3_protocol'] == 'MPLS') & (df_merge['l1_protocol'] == "CABLE_SUBMARINO")].sort_values(
+        (df_merge['l3p'] == 'MPLS') & (df_merge['l1_protocol'] == "CABLE_SUBMARINO")].sort_values(
         by="util_out", ascending=False).to_dict(
         orient='records')
-    caches = df_merge[df_merge['l3_protocol_attr'] == 'CDN'].sort_values(by="util_in", ascending=False).to_dict(
+    caches = df_merge[df_merge['l3a'] == 'CDN'].sort_values(by="util_in", ascending=False).to_dict(
         orient='records')
 
     return render_template('reporte_internet.html', ipts=ipts, ibps=ibps, mpls_cm=mpls_cm, caches=caches, pnis=pnis)
 
 
 if __name__ == '__main__':
-    app.run(port=80, debug=True)
+    app.run(port=8080, host='127.0.0.1', debug=True)
