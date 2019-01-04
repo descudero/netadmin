8aeea261144f18986cca9f958aa5b0e7a36c41b3
descudero <danielignacio.escudero@gmail.com>
2019-01-04 15:36:06 -0600

    first3

--- web_server.py
+++ web_server.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 import socket
+import datetime
+import calendar
+
 
 import pandas as pd
 
@@ -13,8 +16,38 @@ app.config['MYSQL_DB'] = 'netadmin'
 mysql = MySQL(app)
 
 
-@app.route('/reportes/internet/')
+@app.route('/reportes/internet/', methods=['POST'])
 def reporte_internet():
+    percentile = .95
+    month = datetime.date.today().month
+    year = datetime.date.today().year
+    last_day = calendar.monthrange(year, month)[1]
+    initial_date = ''
+    end_date = ''
+    if request.method == 'POST':
+        if 'initial_date' in request.form:
+            initial_date = request.form['initial_date']
+            if initial_date == '':
+                initial_date = '{year}-{month}-{day} 00:00:00'.format(year=year, month=month, day=1)
+            else:
+                initial_date += ' 00:00:00'
+
+        if 'end_date' in request.form:
+            end_date = request.form['end_date']
+            if end_date == '':
+                end_date = '{year}-{month}-{day} 23:59:59'.format(year=year, month=month, day=last_day)
+            else:
+                end_date += ' 23:59:59'
+
+        if 'percentile' in request.form:
+            try:
+                percentile = float(request.form['percentile'])
+            except:
+                percentile = .95
+    if percentile > .99999:
+        percentile = .95
+    print(initial_date, end_date)
+
     sql = '''select  
        d.hostname as host,
        i.uid,
@@ -31,13 +64,13 @@ def reporte_internet():
        from network_devices as d
        inner join interfaces as i on d.uid = i.net_device_uid 
        inner join interface_states as s on i.uid = interface_uid 
-       where s.state_timestamp >='2019-01-01 00:00:00' and s.state_timestamp <'2019-01-31 00:00:00'
-       '''
+       where s.state_timestamp >='{initial_date}' and s.state_timestamp <'{end_date}'
+       '''.format(initial_date=initial_date, end_date=end_date)
     df = pd.read_sql(sql, con=mysql.connection)
     df2 = df[['host', 'uid', 'inter', 'description', 'l3p', 'l3a', 'l1_protocol', 'data_flow',
               'l1_protocol_attr']].drop_duplicates(subset='uid', keep='first')
     df_proceded = (
-        df.groupby(by=['uid'])['util_out', 'util_in', 'out_gbs', 'in_gbs'].quantile(.95)).round(
+        df.groupby(by=['uid'])['util_out', 'util_in', 'out_gbs', 'in_gbs'].quantile(percentile)).round(
         1).reset_index()
     df_merge = pd.merge(df_proceded, df2, on=['uid'], how='left').sort_values(ascending=False, by=['util_out'])
     df_merge.drop(['uid'], axis=1, inplace=True)
@@ -47,9 +80,13 @@ def reporte_internet():
                     'SERVIDORES_CACHE_TERCEROS': 'util_in',
                     'default': 'util_out'}
     titulo = "REPORTE 95% POR FECHAS DE CAPACIDADES RELACIONADAS A INTERNET UFINET"
-    return render_template('reporte_internet.html',
-                           tables=filter_summary(df_merge, columns, sort_column=sort_columns, titulo=titulo))
+    return render_template('reporte_internet_percentile.html', titulo=titulo,
+                           tables=filter_summary(df_merge, columns, sort_column=sort_columns))
 
+
+@app.route('/test_css')
+def css_test():
+    return render_template('layout.html', titulo="prueba")
 
 @app.route('/reportes/internet/actual')
 def reporte_internet_actual():
