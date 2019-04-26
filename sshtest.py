9be7ab1733012d051b0b73f21d0af00e5950c063
descudero <danielignacio.escudero@gmail.com>
2019-04-26 11:19:58 -0600

    first3

--- sshtest.py
+++ sshtest.py
@@ -32,8 +32,8 @@ link_state = {1: 'up',
 
 _sql_state_columns = {'link_state': '1.3.6.1.2.1.2.2.1.7',
                       'protocol_state': '1.3.6.1.2.1.2.2.1.8',
-                      'input_rate': '1.3.6.1.2.1.2.2.1.10',
-                      'output_rate': '1.3.6.1.2.1.2.2.1.16',
+                      'input_rate': '1.3.6.1.2.1.31.1.1.1.6',
+                      'output_rate': '1.3.6.1.2.1.31.1.1.1.10',
                       'input_errors': '1.3.6.1.2.1.2.2.1.14',
                       'output_errors': '1.3.6.1.2.1.2.2.1.20'}
 
@@ -43,18 +43,31 @@ interface_index = '1.3.6.1.2.1.2.2.1.2'
 mtu = '1.3.6.1.2.1.2.2.1.4'
 bandwidth = '1.3.6.1.2.1.31.1.1.1.15'
 
-isp = ISP()
-isp.master = Master()
 host = '172.16.30.250'
-real_oid = '1.3.6.1.2.1.2.2.1.10'
+real_oid = '1.3.6.1.2.1.31.1.1.1.6'
 community = 'INTERNET_UFINET'
-device = CiscoXR(ip=host, display_name='a', master=isp.master)
+# device = CiscoXR(ip=host, display_name='a', master=isp.master)
 # device.set_snmp_bgp_neighbors(special_community="INTERNET_UFINET",address_family=['ipv4','ipv6'])
 
-# data =get_bulk_real_auto(target=host,oid=real_oid,credentials=community)
-# pprint(data)
+data1 = get_bulk_real_auto(target=host, oid=real_oid, credentials=community)
+time1 = time.time()
+time.sleep(15)
+data2 = get_bulk_real_auto(target=host, oid=real_oid, credentials=community)
+time2 = time.time()
+timelapse = float(time2 - time1)
+print(timelapse)
 
 
-print(isp.master.password)
-print(isp.master.username)
-print(Master.encode(key='pinf con vrf', hash='Ufinet18_2'))
+def calculate_delta(old_counter, new_counter, timelapse):
+    print(old_counter)
+    print(new_counter)
+    print(timelapse)
+    new_counter = new_counter if new_counter >= old_counter else new_counter + 18446744073709551616
+    return (new_counter - old_counter) / timelapse
+
+
+data3 = [[registro[0], calculate_delta(new_counter=registro[1],
+                                       old_counter=data1[index][1],
+                                       timelapse=timelapse) * 8]
+         for index, registro in enumerate(data2)]
+pprint(data3[1])
